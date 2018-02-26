18ea7bf94fec5f3b44191fabfd01aaf8cae5a962
Santiago Gualotuña <santiagogf11@gmail.com>
2018-02-25 22:45:21 -0500

    deleted some components, and added services, guards and new components

--- Login/Login/ClientApp/app/app.shared.module.ts
+++ Login/Login/ClientApp/app/app.shared.module.ts
@@ -5,18 +5,18 @@ import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './components/app/app.component';
-import { NavMenuComponent } from './components/navmenu/navmenu.component';
-import { HomeComponent } from './components/home/home.component';
-import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
-import { CounterComponent } from './components/counter/counter.component';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        NavMenuComponent,
-        CounterComponent,
-        FetchDataComponent,
-        HomeComponent
+        LoginComponent,
+        DashboardComponent,
+        FooterComponent,
+        HeaderComponent
     ],
     imports: [
         CommonModule,
@@ -24,9 +24,6 @@ import { CounterComponent } from './components/counter/counter.component';
         FormsModule,
         RouterModule.forRoot([
             { path: '', redirectTo: 'home', pathMatch: 'full' },
-            { path: 'home', component: HomeComponent },
-            { path: 'counter', component: CounterComponent },
-            { path: 'fetch-data', component: FetchDataComponent },
             { path: '**', redirectTo: 'home' }
         ])
     ]
